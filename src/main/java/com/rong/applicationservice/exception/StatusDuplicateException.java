3c9348d3193ddb62760cff01d56f71881443135a
Maknight233 <90875289+Maknight233@users.noreply.github.com>
2025-07-15 17:18:34 -0400

    done for add security

--- src/main/java/com/rong/applicationservice/exception/StatusDuplicateException.java
+++
@@ -1,7 +0,0 @@
-package com.rong.applicationservice.exception;
-
-public class StatusDuplicateException extends RuntimeException {
-    public StatusDuplicateException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/rong/applicationservice/exception/StatusException.java
@@ -0,0 +1,7 @@
+package com.rong.applicationservice.exception;
+
+public class StatusException extends RuntimeException {
+    public StatusException(String message) {
+        super(message);
+    }
+}
